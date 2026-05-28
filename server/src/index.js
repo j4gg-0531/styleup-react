7dc8fcf0fb79a79a3f56ba3190567d0496ac8303
AnotherFedeer <isaac.angulo112@gmail.com>
2026-05-28 17:06:59 -0500

    Express server

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const { PrismaClient } = require('@prisma/client')
+
+const app = express()
+const prisma = new PrismaClient()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Servidor StyleUp funcionando ✅' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
